8f82f98f54c40651bc57bc9b7656ae4445545f8d
Da0111zk <da.azocard@duocuc.cl>
2026-06-30 18:10:02 -0400

    Fix: Dockerfiles y Application.yaml

--- src/main/java/com/example/ingresos/IngresoApplication.java
+++ src/main/java/com/example/ingresos/IngresoApplication.java
@@ -2,8 +2,10 @@ package com.example.ingresos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IngresoApplication {
 
     public static void main(String[] args) {
